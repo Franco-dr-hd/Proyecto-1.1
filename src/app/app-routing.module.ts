f4be19a9ad88297f5b052a183597f362eaaa409f
Franco-dr-hd <derenzisjeanfranco@gmail.com>
2024-05-10 09:50:18 -0300

    cambios

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './modules/inicio/pages/inicio/inicio.component';
+
+const routes: Routes = [
+  // Ruta comun -> componente
+  {
+    path: "", component: InicioComponent
+  },
+  // Carga PEREZOSA -> ruta que te lleva a un módulo específico 
+  {
+    path: "", loadChildren: () => import('./modules/inicio/inicio.module').then(m => m.InicioModule)
+  }
+  // loadchildren: indica que habrá una ruta hija
+  // ()=> import: funcion flecha que importa desde la ruta
+  // .then: Funcion asíncronica del tipo PROMESA 
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
